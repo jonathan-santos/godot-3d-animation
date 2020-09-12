3c6eb60c4525d009b4807fa7785f60cacd7c0fa0
Jonathan Santos <jonathan.santosaugusto@gmail.com>
2020-09-12 19:01:49 -0300

    Project started with main scene and moving player

---
+++ scripts/player.gd
@@ -0,0 +1,24 @@
+extends KinematicBody
+
+const SPEED := 15
+const ACCELERATION := 20.0
+var velocity: Vector3
+var direction: Vector3
+
+onready var camera: Spatial = get_viewport().get_camera()
+
+func _process(delta):
+	direction = Vector3()
+	
+	if Input.is_action_pressed("move_forward"):
+		direction -= camera.transform.basis.z
+	elif Input.is_action_pressed("move_backward"):
+		direction += camera.transform.basis.z
+		
+	if Input.is_action_pressed("move_left"):
+		direction -= camera.transform.basis.x
+	elif Input.is_action_pressed("move_right"):
+		direction += camera.transform.basis.x
+	
+	velocity = velocity.linear_interpolate(direction * SPEED, ACCELERATION * delta)
+	velocity = move_and_slide(velocity, Vector3.UP)
